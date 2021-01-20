3e225a16cc877bedb997d8727fface1509be363d
Ryley <ryley.g@hotmail.com>
2021-01-19 22:34:21 -0700

    fixed SQL syntax in create_games

--- tables.py
+++ tables.py
@@ -1,4 +1,3 @@
-import sqlite3
 from utilities import connection
 
 def create_games():
@@ -7,13 +6,20 @@ def create_games():
     id INTEGER UNIQUE PRIMARY KEY AUTOINCREMENT,
     date_played TEXT,
     game_played TEXT,
-    FOREIGN KEY (id) REFERENCES players (id),
-    FOREIGN KEY (id) REFERENCES players (id),
-    FOREIGN KEY (id) REFERENCES players (id),
-    FOREIGN KEY (id) REFERENCES players (id),
-    FOREIGN KEY (id) REFERENCES players (id),
-    FOREIGN KEY (id) REFERENCES players (id),
-    FOREIGN KEY (id) REFERENCES players (id)
+    player1 TEXT,
+    player2 TEXT,
+    player3 TEXT,
+    player4 TEXT,
+    player5 TEXT,
+    player6 TEXT,
+    winner TEXT,
+    FOREIGN KEY (player1) REFERENCES players (id),
+    FOREIGN KEY (player2) REFERENCES players (id),
+    FOREIGN KEY (player3) REFERENCES players (id),
+    FOREIGN KEY (player4) REFERENCES players (id),
+    FOREIGN KEY (player5) REFERENCES players (id),
+    FOREIGN KEY (player6) REFERENCES players (id),
+    FOREIGN KEY (winner) REFERENCES players (id)
     )'''
     conn, c = connection()
     c.execute(create_games_query)
@@ -24,13 +30,9 @@ def create_players():
     create_players_query = '''
         CREATE TABLE if not exists players(
         id INTEGER UNIQUE PRIMARY KEY AUTOINCREMENT,
-        first_name TEXT,
-        last_name TEXT
+        name TEXT
         )'''
     conn, c = connection()
     c.execute(create_players_query)
     conn.commit()
     conn.close()
-
-def create_tables():
-    pass
