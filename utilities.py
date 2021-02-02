64ad8c223678eec8ba285601feaf9447558d030b
Ryley <ryley.g@hotmail.com>
2021-02-01 23:08:00 -0700

    minor updates to edit_game and list_games

--- utilities.py
+++ utilities.py
@@ -31,12 +31,14 @@ def list_games():
     # TODO: improve readability of game listing
     name_query = 'SELECT * from players'
     c.execute(name_query)
+    # creates a dict of id:name from the name query
     names_dict = {player_row[0]: (' '.join(player_row[1:3])) for player_row in c.fetchall()}
     if games:
         print('List of games:')
-        print('''Date of Game    Game Played    Player     Player      Player      Player      Player      Player      Winner''')
+        print('''ID     Date of Game    Game Played    Player     Player      Player      Player      Player      Player      Winner''')
         for row in games:
             game_data = [
+            str(row[0]),
             row[1],
             row[2],
             names_dict.get(row[3],' - '),
@@ -61,7 +63,7 @@ def edit_player():
         try:
             player_to_edit = int(input('Please enter the id of the player you would like to edit:  '))
         except ValueError:
-            print('Please enter an integer number of players between 2 and 6.')
+            print('Please enter the player id as an integer.')
             continue
         finally:
             if player_to_edit is None or player_to_edit not in existing_players:
@@ -98,7 +100,124 @@ def edit_player():
 
 
 def edit_game():
-    pass
+    conn, c = connection()
+    list_games()
+    c.execute('Select id from games;')
+    existing_games = [i[0] for i in c.fetchall()]
+    while True:
+        try:
+            game_to_edit = int(input('Please enter the id of the game you would like to edit (100 to abort):  '))
+        except ValueError:
+            print('Please enter the game id as an integer.')
+            continue
+        finally:
+            if game_to_edit is None or game_to_edit not in existing_games:
+                print('Please enter a game from the database.')
+                continue
+            elif game_to_edit == 100:
+                conn.close()
+                return None
+            else:
+                break
+    print('Please enter revised data for this game as follows: ')
+    while True:
+        game_played = input('Enter game: ').strip().title()
+        # split and re-join game by words in case there are extra spaces
+        game_played = ' '.join(game_played.split())
+        date_played = input('Enter date of game (format YYYY-MM-DD): ').strip()
+        date_ok = validate_date(date_played)
+        game_ok = validate_game(game_played)
+        if date_ok:
+            if game_ok:
+                break
+            else:
+                print('There was an error with the game name, please try again:')
+                continue
+        else:
+            print('There was an error with the date, please try again (format YYYY-MM-DD):')
+            continue
+    while True:
+        try:
+            num_players = int(input('Enter the number of players (2 - 6): '))
+        except ValueError:
+            print('Please enter an integer number of players between 2 and 6.')
+            continue
+        finally:
+            if not 2 <= num_players <= 6:
+                print('Please enter an integer number of players between 2 and 6.')
+                continue
+            else:
+                break
+    player_ids = []
+    # print list of players, get player ids, validate they are in database already
+    list_players()
+    c.execute('Select id from players;')
+    existing_players = [i[0] for i in c.fetchall()]
+    # while loop for getting names of players
+    while len(player_ids) != num_players:
+        print('Please enter the id of the player.  If they are not listed, '
+              'please enter them into the database first (enter 100 to quit back to menu).')
+        try:
+            p = int(input())
+            if p == 100:
+                break
+        except ValueError:
+            print('Please enter only an integer for the id.\n')
+            continue
+        if p not in existing_players:
+            print('Please enter the player in the database before adding this game.\n')
+        elif p in player_ids:
+            print('You have already entered that player.  Try again.\n')
+        else:
+            player_ids.append(p)
+    # get the winner from user
+    list_players()
+    while True:
+        try:
+            winner = int(input('Please enter the id of the winner.  If they are not listed, '
+                               'please enter them into the database first (enter 100 to quit back to menu)'))
+        except ValueError:
+            print('Please enter only an integer for the id.\n')
+            continue
+        if winner == 100:
+            conn.close()
+            return None
+        elif winner not in existing_players:
+            print('Please enter the player in the database before adding this game.\n')
+            continue
+        elif winner not in player_ids:
+            print('The winner must have played the game! Try again.\n')
+            continue
+        else:
+            break
+    query = f'UPDATE games set ' \
+            f'date_played = ? ' \
+            f'game_played = ? ' \
+            f'player_ids[0] = ?, ' \
+            f'player_ids[1] = ?' \
+            f'player_ids[2] = ?'\
+            f'player_ids[3] = ?' \
+            f'player_ids[4] = ?' \
+            f'player_ids[5] = ?' \
+            f'winner = ? '\
+            f'WHERE id = ?'
+    nulls = ['NULL' for _ in range(6 - len(player_ids))]
+    player_ids = player_ids + nulls
+    values = (date_played,
+              game_played,
+              player_ids[0],
+              player_ids[1],
+              player_ids[2],
+              player_ids[3],
+              player_ids[4],
+              player_ids[5],
+              winner,
+              )
+    #c.execute(query, values)
+    #conn.commit()
+    #conn.close()
+    print(query, values, sep='\n')
+    print('Game successfully updated!')
 
 
 def add_new_player():
@@ -236,7 +355,7 @@ def add_new_game():
     conn.commit()
     conn.close()
     print('Game successfully added!')
-    # print(player_ids, values, query, sep='\n')
+
 
 
 def delete_games():
